33f5eb19014ccdf3889973b51f076204f967b0d6
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-26 15:41:38 -0300

    Ainda nao concluir

---
+++ Denner/api/api_serverRest/src/main/java/Utils/EndPoint.java
@@ -0,0 +1,8 @@
+package Utils;
+
+public class EndPoint {
+
+//    protected static final String EMAIL = ObjectsUtils.getPropertiesData("dadoLogin", "email");
+//    protected static final String PASSWORD = ObjectsUtils.getPropertiesData("dadoLogin", "password");
+
+}
