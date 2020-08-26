b96621829495dde1e7b4ca629425259d426d07cb
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-26 16:33:39 -0300

    Login para test case

---
+++ Jeferson/API/api_tarefas/src/test/java/TestCase/LoginTestCase.java
@@ -0,0 +1,41 @@
+package TestCase;
+
+import BaseTest.LoginBaseTest;
+import Utils.ObjectsUtils;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class LoginTestCase extends LoginBaseTest {
+
+    @Test
+    public void login(){
+        String token =
+        given()
+                .log().all()
+                .spec(requestSpec)
+        .when()
+                .post()
+        .then()
+                .log().all()
+                .spec(responseSpec)
+                .extract().path("data.attributes.auth-token");
+
+        ObjectsUtils.setPropertiesData("dadosInformacao", "token", token);
+        logout();
+
+    }
+
+    public void logout(){
+        String token = ObjectsUtils.getPropertiesData("dadosInformacao", "token");
+        given()
+                .log().all()
+                .spec(requestSpec)
+        .when()
+                .delete("/"+token)
+        .then()
+                .log().all()
+                .statusCode(204);
+        System.out.println("sucesso sair do login");
+    }
+}
