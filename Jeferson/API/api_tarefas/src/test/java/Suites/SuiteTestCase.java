1facdbf5a926adeb5bd5e7685ac0515a14eb5670
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-27 15:40:49 -0300

    Selecionei alguns classes para executar

--- Jeferson/API/api_tarefas/src/test/java/Suites/SuiteTestCase.java
+++ Jeferson/API/api_tarefas/src/test/java/Suites/SuiteTestCase.java
@@ -1,9 +1,6 @@
 package Suites;
 
-import TestCase.CreateTaskTestCase;
-import TestCase.ListTaskTestCase;
-import TestCase.LoginTestCase;
-import TestCase.LogoutTestCase;
+import TestCase.*;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.SelectPackages;
@@ -11,7 +8,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
 //@SelectPackages("TestCase")
-@SelectClasses({LoginTestCase.class, CreateTaskTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
+//@SelectClasses({LoginTestCase.class, CreateTaskTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
+@SelectClasses({LoginTestCase.class, EditTaskTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
+//@SelectClasses({LoginTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
 public class SuiteTestCase {
 
 }
