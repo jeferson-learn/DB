6d5ee7f621ab30d212b72fd8f0675b22077c7bd6
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-27 14:47:09 -0300

    Criei request e response parte tarefa e adicionou dado tarefa

--- Jeferson/API/api_tarefas/src/main/java/BaseTest/CreateTaskBaseTest.java
+++ Jeferson/API/api_tarefas/src/main/java/BaseTest/CreateTaskBaseTest.java
@@ -1,8 +1,40 @@
 package BaseTest;
 
+import Models.CreateTaskModel;
 import Utils.EndPoints;
+import Utils.Maps;
+import Utils.ObjectsUtils;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Map;
 
 public class CreateTaskBaseTest extends EndPoints {
 
+    protected static RequestSpecification requestSpec;
+    protected static ResponseSpecification responseSpec;
+    private static Map<String, Object> objectMap;
+
+    @BeforeEach
+    public void setUp(){
+        String token = ObjectsUtils.getPropertiesData("dadosInformacao", "token");
+        objectMap = Maps.objectMap("task", new CreateTaskModel());
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setBasePath(BASE_PATH_TASKS)
+                .addHeader("Authorization", token)
+                .setContentType(ContentType.JSON)
+                .setBody(objectMap)
+                .build();
+
+        responseSpec = new ResponseSpecBuilder()
+                .expectStatusCode(201)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
 
 }
