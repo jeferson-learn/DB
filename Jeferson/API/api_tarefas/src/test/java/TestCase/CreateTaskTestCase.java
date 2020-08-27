530be97de61aa95f3407480a67304a1c523d08b8
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-27 14:43:51 -0300

    Adicionei metodo para fez criar um tarefa

--- Jeferson/API/api_tarefas/src/test/java/TestCase/CreateTaskTestCase.java
+++ Jeferson/API/api_tarefas/src/test/java/TestCase/CreateTaskTestCase.java
@@ -1,4 +1,20 @@
 package TestCase;
 
-public class CreateTaskTestCase {
+import BaseTest.CreateTaskBaseTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class CreateTaskTestCase extends CreateTaskBaseTest {
+
+    @Test
+    public void listarTarefas(){
+        given()
+                .spec(requestSpec)
+        .when()
+                .post()
+        .then()
+                .log().body()
+                .spec(responseSpec);
+    }
 }
