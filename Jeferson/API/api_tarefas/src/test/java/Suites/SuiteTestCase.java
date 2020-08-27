e7856456962afd5202eefc186b3adc0fe2f61cfe
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-27 15:57:27 -0300

    Adicionei classe deletar, com suite outros classe para executar

--- Jeferson/API/api_tarefas/src/test/java/Suites/SuiteTestCase.java
+++ Jeferson/API/api_tarefas/src/test/java/Suites/SuiteTestCase.java
@@ -6,11 +6,13 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
+
 @RunWith(JUnitPlatform.class)
 //@SelectPackages("TestCase")
 //@SelectClasses({LoginTestCase.class, CreateTaskTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
-@SelectClasses({LoginTestCase.class, EditTaskTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
+//@SelectClasses({LoginTestCase.class, EditTaskTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
 //@SelectClasses({LoginTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
+@SelectClasses({LoginTestCase.class, DeleteTaskTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
 public class SuiteTestCase {
 
 }
