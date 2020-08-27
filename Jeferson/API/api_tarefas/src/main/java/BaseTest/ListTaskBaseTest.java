7c1498b26f87c7d130bf57342188683ce9a4aec4
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-27 11:54:36 -0300

    Criei classe listar tarefa base teste

---
+++ Jeferson/API/api_tarefas/src/main/java/BaseTest/ListTaskBaseTest.java
@@ -0,0 +1,33 @@
+package BaseTest;
+
+import Utils.EndPoints;
+import Utils.ObjectsUtils;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.BeforeAll;
+
+public class ListTaskBaseTest extends EndPoints {
+
+    protected static RequestSpecification requestSpec;
+    protected static ResponseSpecification responseSpec;
+
+    @BeforeAll
+    public void setUp(){
+        String token = ObjectsUtils.getPropertiesData("dadosInformacao", "token");
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setBasePath(BASE_PATH_TASKS)
+                .addHeader("Authorization", token)
+                .setContentType(ContentType.JSON)
+                .build();
+        System.out.println(token);
+        responseSpec = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+
+}
