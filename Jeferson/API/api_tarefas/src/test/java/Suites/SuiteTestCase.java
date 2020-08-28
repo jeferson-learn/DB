edcb6a04f191360125430e2c6247eec238be05d7
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-28 18:57:09 -0300

    Criei classe suite para executar todos classes

--- Jeferson/API/api_tarefas/src/test/java/Suites/SuiteTestCase.java
+++ Jeferson/API/api_tarefas/src/test/java/Suites/SuiteTestCase.java
@@ -8,11 +8,8 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(JUnitPlatform.class)
-//@SelectPackages("TestCase")
-//@SelectClasses({LoginTestCase.class, CreateTaskTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
-//@SelectClasses({LoginTestCase.class, EditTaskTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
+@SelectPackages("TestCase")
 //@SelectClasses({LoginTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
-@SelectClasses({LoginTestCase.class, DeleteTaskTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
 public class SuiteTestCase {
 
 }
