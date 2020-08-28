140390dd76cedba6336c0626c8d872235aba06b8
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-28 18:59:46 -0300

    Adicionei anotacoes para fazer relatorio allure

--- Jeferson/API/api_tarefas/src/test/java/TestCase/LoginTestCase.java
+++ Jeferson/API/api_tarefas/src/test/java/TestCase/LoginTestCase.java
@@ -2,12 +2,20 @@ package TestCase;
 
 import BaseTest.LogarBaseTest;
 import Utils.ObjectsUtils;
+import com.sun.org.glassfish.gmbal.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 
 public class LoginTestCase extends LogarBaseTest {
 
+    @Feature("API de tarefa 'login'")
+    @DisplayName("LOGAR")
+    @Description("Logar da conta")
+    @Issue("https://api-de-tarefas.herokuapp.com/sessions")
     @Test
     public void login(){
         String token =
