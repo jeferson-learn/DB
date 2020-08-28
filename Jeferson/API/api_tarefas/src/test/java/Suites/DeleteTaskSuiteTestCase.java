29f2f177afb212fd57086022ab03fa117e036533
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-28 18:53:28 -0300

    Criei classe suite para executar deletar dado tarefa

---
+++ Jeferson/API/api_tarefas/src/test/java/Suites/DeleteTaskSuiteTestCase.java
@@ -0,0 +1,15 @@
+package Suites;
+
+import TestCase.DeleteTaskTestCase;
+import TestCase.ListTaskTestCase;
+import TestCase.LoginTestCase;
+import TestCase.LogoutTestCase;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({LoginTestCase.class, DeleteTaskTestCase.class, ListTaskTestCase.class, LogoutTestCase.class})
+public class DeleteTaskSuiteTestCase {
+
+}
