0867ed4a9b6b1a3a70fff808f91d863e85577405
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-28 18:59:24 -0300

    Adicionei anotacoes para fazer relatorio allure

--- Jeferson/API/api_tarefas/src/test/java/TestCase/DeleteTaskTestCase.java
+++ Jeferson/API/api_tarefas/src/test/java/TestCase/DeleteTaskTestCase.java
@@ -1,18 +1,26 @@
 package TestCase;
 
 import BaseTest.DeleteTaskBaseTest;
+import com.sun.org.glassfish.gmbal.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class DeleteTaskTestCase extends DeleteTaskBaseTest {
 
+    @Feature("API de tarefa 'delete'")
+    @DisplayName("APAGAR")
+    @Description("Apaga um dado tarefa da conta")
+    @Issue("https://api-de-tarefas.herokuapp.com/tasks")
     @Test
     public void DeleteTarefas(){
         given()
                 .spec(requestSpec)
         .when()
-                .delete("/492")
+                .delete("/491")
         .then()
                 .log().body()
                 .spec(responseSpec);
