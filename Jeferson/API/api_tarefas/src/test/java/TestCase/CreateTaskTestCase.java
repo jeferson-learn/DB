06f5ad5a158c8fa320236da1f5ffe2331d150cde
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-28 18:59:16 -0300

    Adicionei anotacoes para fazer relatorio allure

--- Jeferson/API/api_tarefas/src/test/java/TestCase/CreateTaskTestCase.java
+++ Jeferson/API/api_tarefas/src/test/java/TestCase/CreateTaskTestCase.java
@@ -1,12 +1,20 @@
 package TestCase;
 
 import BaseTest.CreateTaskBaseTest;
+import com.sun.org.glassfish.gmbal.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class CreateTaskTestCase extends CreateTaskBaseTest {
 
+    @Feature("API de tarefa 'create'")
+    @DisplayName("CRIAR TAREFA")
+    @Description("Criar um dado tarefa da conta")
+    @Issue("https://api-de-tarefas.herokuapp.com/tasks")
     @Test
     public void listarTarefas(){
         given()
