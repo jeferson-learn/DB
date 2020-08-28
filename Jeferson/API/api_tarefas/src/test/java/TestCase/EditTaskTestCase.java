340ddf0d5154296646c805c4da87835bab224601
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-28 20:04:55 -0300

    Adicionei metodo Json validar

--- Jeferson/API/api_tarefas/src/test/java/TestCase/EditTaskTestCase.java
+++ Jeferson/API/api_tarefas/src/test/java/TestCase/EditTaskTestCase.java
@@ -4,8 +4,11 @@ import BaseTest.EditTaskBaseTest;
 import com.sun.org.glassfish.gmbal.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Issue;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.given;
 
@@ -23,6 +26,7 @@ public class EditTaskTestCase extends EditTaskBaseTest {
                 .put()
         .then()
                 .log().body()
-                .spec(responseSpec);
+                .spec(responseSpec)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("Schemas" + File.separator + "EditarTarefaJsonSchemas.json"));
     }
 }
