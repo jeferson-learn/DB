d8d155eac0ce71b43ffcab3aed0c4790b28d983d
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-08-28 19:00:11 -0300

    Adicionei anotacoes para fazer relatorio allure

--- Jeferson/API/api_tarefas/src/test/java/TestCase/CreateUserTestCase.java
+++ Jeferson/API/api_tarefas/src/test/java/TestCase/CreateUserTestCase.java
@@ -1,12 +1,20 @@
 package TestCase;
 
 import BaseTest.CreateUserBaseTest;
+import com.sun.org.glassfish.gmbal.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 
 public class CreateUserTestCase extends CreateUserBaseTest {
 
+    @Feature("API de tarefa 'create user'")
+    @DisplayName("CRIAR USUARIO")
+    @Description("Cria uma conta da tarefa")
+    @Issue("https://api-de-tarefas.herokuapp.com/users")
     @Test
     public void createUser(){
         given()
