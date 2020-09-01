fbe5d55ee744a4cd5d08ca198d9ba8b9177da155
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-09-01 14:03:19 -0300

    Adicionei anotacao para pegar xml body

--- Udemy/api com Rest Assured/udemyAPIcomRestAssured/src/test/java/serialização/User.java
+++ Udemy/api com Rest Assured/udemyAPIcomRestAssured/src/test/java/serialização/User.java
@@ -1,11 +1,24 @@
 package serialização;
 
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="user")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class User {
 
+    @XmlAttribute
     private Long id;
     private String name;
     private Integer age;
     private Double salary;
+
+    public User(){
+
+    }
 
     public User(String name, Integer age) {
         super();
