fb9b107af185d343d84f94ea634ea90cd3ac712a
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-09-08 16:47:33 -0300

    Criei classe Map, fazer criar um objeto

---
+++ Udemy/api com Rest Assured/udemyAPIcomRestAssured/src/main/java/barrigaFW/Maps.java
@@ -0,0 +1,14 @@
+package barrigaFW;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Maps {
+
+    public void createObject(){
+        Map<String, String> object = new HashMap<String, String>();
+        object.put("email", "jefersonbarriga@hotmail.com");
+        object.put("senha", "123456");
+    }
+
+}
