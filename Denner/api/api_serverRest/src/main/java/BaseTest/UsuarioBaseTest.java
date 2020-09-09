d9b0d51c114a585e0801540c1835f34abc36e704
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-09-09 16:35:13 -0300

    Criei classe usuario base test

---
+++ Denner/api/api_serverRest/src/main/java/BaseTest/UsuarioBaseTest.java
@@ -0,0 +1,28 @@
+package BaseTest;
+
+import Utils.EndPoint;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.BeforeEach;
+
+public class UsuarioBaseTest extends EndPoint {
+
+    protected static RequestSpecification requestSpec;
+    protected static ResponseSpecification responseSpec;
+
+    @BeforeEach
+    public void setUp(){
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setBasePath(PATH_USUARIOS)
+                .setContentType(ContentType.JSON)
+                .build();
+
+        responseSpec = new ResponseSpecBuilder()
+                .expectStatusCode(201)
+                .build();
+    }
+}
