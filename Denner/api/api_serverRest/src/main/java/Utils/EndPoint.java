aaa03d4de82ed8fdcd74c3661b141d6ef19db985
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-09-09 16:41:27 -0300

    Adicionei informacao dados pra variaveis

--- Denner/api/api_serverRest/src/main/java/Utils/EndPoint.java
+++ Denner/api/api_serverRest/src/main/java/Utils/EndPoint.java
@@ -2,7 +2,16 @@ package Utils;
 
 public class EndPoint {
 
-//    protected static final String EMAIL = ObjectsUtils.getPropertiesData("dadoLogin", "email");
-//    protected static final String PASSWORD = ObjectsUtils.getPropertiesData("dadoLogin", "password");
+    protected static final String NOME = ObjectsUtils.getPropertiesData("dadoLogin", "nome");
+    protected static final String EMAIL = ObjectsUtils.getPropertiesData("dadoLogin", "email");
+    protected static final String PASSWORD = ObjectsUtils.getPropertiesData("dadoLogin", "password");
+    protected static final String ADM = ObjectsUtils.getPropertiesData("dadoLogin", "administrador");
+
+    protected static final String EMAILEXISTE = ObjectsUtils.getPropertiesData("dadoLogin", "emailExiste");
+    protected static final String PASSWORDEXISTE = ObjectsUtils.getPropertiesData("dadoLogin", "passwordExiste");
+
+    protected static final String BASE_URI = ObjectsUtils.getPropertiesData("dadosEndPoint", "URI");
+    protected static final String PATH_LOGIN = ObjectsUtils.getPropertiesData("dadosEndPoint", "LOGIN");
+    protected static final String PATH_USUARIOS = ObjectsUtils.getPropertiesData("dadosEndPoint", "USUARIOS");
 
 }
