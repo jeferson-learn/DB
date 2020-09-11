07702e2c710cea990e2362cc5f64a9cefbaeef2e
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-09-10 21:58:11 -0300

    Adicionei metodos para criar objetos

--- Denner/api/api_serverRest/src/main/java/Utils/ObjectJson.java
+++ Denner/api/api_serverRest/src/main/java/Utils/ObjectJson.java
@@ -14,11 +14,21 @@ public class ObjectJson extends EndPoint {
     private static String createAdm = ADM;
 
 
+    public static String loginTest(){
+        JSONObject login = new JSONObject();
+        login.put("nome", createNome);
+        login.put("email", createEmail);
+        login.put("password", createPassword);
+        login.put("administrador", createAdm);
+
+        return login.toString();
+    }
+
     public static String loginExisteJsonObject(){
 
         JSONObject login = new JSONObject();
-        login.put("email", createEmail);
-        login.put("password", createPassword);
+        login.put("email", email);
+        login.put("password", password);
 
         return login.toString();
     }
@@ -30,6 +40,37 @@ public class ObjectJson extends EndPoint {
         login.put("email", createEmail);
         login.put("password", createPassword);
         login.put("administrador", createAdm);
+
+        return login.toString();
+    }
+
+    public static String loginNewJsonObject(){
+
+        JSONObject login = new JSONObject();
+        login.put("email", createEmail);
+        login.put("password", createPassword);
+
+        return login.toString();
+    }
+
+    public static String createUser(){
+
+        JSONObject login = new JSONObject();
+        login.put("nome", "Jeferson Lopes Eugenio");
+        login.put("email", "jeferson1@teste.com");
+        login.put("password", "123456");
+        login.put("administrador", "true");
+
+        return login.toString();
+    }
+
+    public static String editUser(){
+
+        JSONObject login = new JSONObject();
+        login.put("nome", "Jeferson Nascimento Eugenio");
+        login.put("email", "jeferson1@teste.com");
+        login.put("password", "123456");
+        login.put("administrador", "true");
 
         return login.toString();
     }
