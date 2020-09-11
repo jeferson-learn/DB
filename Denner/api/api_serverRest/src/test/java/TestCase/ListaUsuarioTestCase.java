89b7942482f9a9d7214f68d29d38c2cc79ce9502
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-09-10 22:06:21 -0300

    Fazer lista do usuario

--- Denner/api/api_serverRest/src/test/java/TestCase/ListaUsuarioTestCase.java
+++ Denner/api/api_serverRest/src/test/java/TestCase/ListaUsuarioTestCase.java
@@ -1,4 +1,33 @@
 package TestCase;
 
-public class ListaUsuarioTestCase {
+import BaseTest.UsuarioBaseTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class ListaUsuarioTestCase extends UsuarioBaseTest {
+
+    @Test
+    public void listaUsuario(){
+
+        System.out.println("Lista usuario");
+
+        String id = Utils.ObjectsUtils.getPropertiesData("dadosUsuario", "ID");
+
+        given()
+                .spec(requestSpec)
+//                .queryParam("_id", id)
+//                .queryParam("_id", "b0hqD49Pu7eHNhj9")
+//                .queryParam("_id", "Twf8t49HykiPdj4P")
+//                .queryParam("_id", "aoiEH7nPiTUpaEiC")
+//                .queryParam("email", "fulano@qa.com")
+//                .queryParam("email", "jeferson1@teste.com")
+//                .queryParam("password", "teste")
+        .when()
+                .get()
+        .then()
+                .log().body()
+                .statusCode(200)
+                .spec(responseSpec);
+    }
 }
