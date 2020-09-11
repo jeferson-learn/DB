954910dc87ce515f7513caeb7a8beaf3a15fcf8a
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-09-10 22:01:55 -0300

    Seleciona cada classe para executar um vez

--- Denner/api/api_serverRest/src/test/java/Suite/SuiteTest.java
+++ Denner/api/api_serverRest/src/test/java/Suite/SuiteTest.java
@@ -1,4 +1,12 @@
 package Suite;
 
+import TestCase.*;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({LoginTestCase.class, CadastrarLogarTestCase.class, ListaUsuarioTestCase.class, EditarUsuarioTestCase.class, DeletarUsuario.class})
 public class SuiteTest {
+
 }
