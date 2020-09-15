2d79148ae8a73b65d9470a720e992fd7f2749608
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-09-15 10:49:27 -0300

    Adicionei metodos request e response

--- Denner/api/api_serverRest/src/main/java/BaseTest/CarrinhoBaseTest.java
+++ Denner/api/api_serverRest/src/main/java/BaseTest/CarrinhoBaseTest.java
@@ -1,9 +1,36 @@
 package BaseTest;
 
 import Utils.EndPoint;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.BeforeEach;
 
 public class CarrinhoBaseTest extends EndPoint {
 
+    protected static RequestSpecification requestSpec;
+    protected static ResponseSpecification responseSpec;
 
+    @BeforeEach
+    public void setUp(){
+        request();
+        response();
+    }
+
+    public void request() {
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setBasePath(PATH_CARRINHOS)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+    public void response() {
+        responseSpec = new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
 
 }
