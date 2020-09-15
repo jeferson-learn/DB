15888d299e5a2911e83127f95e2121d8d7aa5ec1
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-09-15 09:43:53 -0300

    Criei classe deletar produto

---
+++ Denner/api/api_serverRest/src/test/java/TestCase/DeletarProdutoTestCase.java
@@ -0,0 +1,39 @@
+package TestCase;
+
+import BaseTest.ProdutoBaseTest;
+import com.sun.org.glassfish.gmbal.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class DeletarProdutoTestCase extends ProdutoBaseTest {
+
+    @Feature("API de Serverest 'DELETAR'")
+    @DisplayName("DELETAR")
+    @Description("Deleta produto")
+    @Issue("http://localhost:3000/login")
+    @Test
+    public void deletarProduto(){
+
+        System.out.println("Deletar produto");
+
+        String id = Utils.ObjectsUtils.getPropertiesData("dadosProduto", "ID");
+        String token = Utils.ObjectsUtils.getPropertiesData("dadosUsuario", "TOKEN");
+
+        given()
+                .spec(requestSpec)
+                .header("Authorization", token)
+                .pathParam("id", id)
+        .when()
+                .delete("/{id}")
+//                .delete()
+        .then()
+                .log().body()
+                .statusCode(200)
+//                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas" + File.separator + "ListaUsuarioJsonSchemas.json"))
+                .spec(responseSpec);
+    }
+}
