65f4ef84b6143368ca301e71f1361ad13a1b29fd
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-09-16 11:20:49 -0300

    Adicionei import validar json schemas

--- Denner/api/api_serverRest/src/test/java/TestCase/EditarProdutoTestCase.java
+++ Denner/api/api_serverRest/src/test/java/TestCase/EditarProdutoTestCase.java
@@ -5,8 +5,11 @@ import Models.EditarProdutoModel;
 import com.sun.org.glassfish.gmbal.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Issue;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.given;
 
@@ -39,7 +42,7 @@ public class EditarProdutoTestCase extends ProdutoBaseTest {
         .then()
                 .log().body()
                 .statusCode(200)
-//                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas" + File.separator + "ListaUsuarioJsonSchemas.json"))
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas" + File.separator + "EditarProdutoJsonSchemas.json"))
                 .spec(responseSpec);
     }
 }
