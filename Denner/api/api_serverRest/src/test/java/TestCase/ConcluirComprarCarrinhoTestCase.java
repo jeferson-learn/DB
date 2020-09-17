b83ef701efc063cddf28cf89601e68b2e0f8a168
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-09-17 17:28:08 -0300

    Criei classe concluir carrinho

---
+++ Denner/api/api_serverRest/src/test/java/TestCase/ConcluirComprarCarrinhoTestCase.java
@@ -0,0 +1,37 @@
+package TestCase;
+
+import BaseTest.CarrinhoBaseTest;
+import com.sun.org.glassfish.gmbal.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ConcluirComprarCarrinhoTestCase extends CarrinhoBaseTest {
+
+    @Feature("API de Serverest 'Concluir comprar carrinho'")
+    @DisplayName("CONCLUIR COMPRAR CARRINHO")
+    @Description("Concluir comprar do produto carrinho")
+    @Issue("http://localhost:3000/carrinhos/concluir-compra")
+    @Test
+    public void concluirComprarCarrinho(){
+
+        System.out.println("Concluir comprar carrinho");
+
+        String token = Utils.ObjectsUtils.getPropertiesData("dadosUsuario", "TOKEN");
+
+        given()
+                .spec(requestSpec)
+                .header("authorization", token)
+        .when()
+                .delete("/concluir-compra")
+        .then()
+                .log().body()
+                .statusCode(200)
+//                        .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas" + File.separator + "CadastrarProdutoJsonSchemas.json"))
+                .spec(responseSpec);
+
+    }
+}
