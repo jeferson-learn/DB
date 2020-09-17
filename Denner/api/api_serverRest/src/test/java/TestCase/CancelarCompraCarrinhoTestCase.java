4a450b305f3e9ee7ce97a3d42cfe3ce68bef9024
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-09-17 17:27:58 -0300

    Criei classe cancelar carrinho

---
+++ Denner/api/api_serverRest/src/test/java/TestCase/CancelarCompraCarrinhoTestCase.java
@@ -0,0 +1,37 @@
+package TestCase;
+
+import BaseTest.CarrinhoBaseTest;
+import com.sun.org.glassfish.gmbal.Description;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Issue;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class CancelarCompraCarrinhoTestCase extends CarrinhoBaseTest {
+
+    @Feature("API de Serverest 'Cancela comprar carrinho'")
+    @DisplayName("CANCELA COMPRAR CARRINHO")
+    @Description("Cancela comprar do produto carrinho")
+    @Issue("http://localhost:3000/carrinhos/cancelar-compra")
+    @Test
+    public void cancelaComprarCarrinho(){
+
+        System.out.println("Cancela comprar carrinho");
+
+        String token = Utils.ObjectsUtils.getPropertiesData("dadosUsuario", "TOKEN");
+
+            given()
+                    .spec(requestSpec)
+                    .header("authorization", token)
+            .when()
+                    .delete("/cancelar-compra")
+            .then()
+                    .log().body()
+                    .statusCode(200)
+//                        .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemas" + File.separator + "CadastrarProdutoJsonSchemas.json"))
+                    .spec(responseSpec);
+
+    }
+}
