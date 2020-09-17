306f1f91b5b56e1c4163fded5d7cf18ef4972907
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-09-17 17:27:08 -0300

    Criei classe suite para executar classes para um vez

---
+++ Denner/api/api_serverRest/src/test/java/Suite/CarrinhosSuiteTestCase.java
@@ -0,0 +1,21 @@
+package Suite;
+
+import TestCase.*;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({
+        CadastrarUsuarioTestCase.class,
+        LoginTestCase.class,
+        CadastrarCarrinhoTestCase.class,
+        ListaCarrinhoTestCase.class,
+//        ConcluirComprarCarrinhoTestCase.class,
+        CancelarCompraCarrinhoTestCase.class,
+        DeletarUsuarioTestCase.class
+})
+public class CarrinhosSuiteTestCase {
+
+}
+
