dec6846d96de165e61d9257fe06f1de9159d86a6
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-11-06 12:39:11 -0300

    Coloquei metodo parte numero para teste

--- Jeferson/teste/gerarFaker/src/test/java/testCase/FakerTestCase.java
+++ Jeferson/teste/gerarFaker/src/test/java/testCase/FakerTestCase.java
@@ -3,6 +3,7 @@ package testCase;
 import endereco.EnderecoFakers;
 import internet.InternetFakers;
 import nomes.NomesFakers;
+import numeros.NumeroFakers;
 import org.junit.jupiter.api.Test;
 
 public class FakerTestCase {
@@ -10,6 +11,12 @@ public class FakerTestCase {
     NomesFakers nomesFakers = new NomesFakers();
     InternetFakers internetFakers = new InternetFakers();
     EnderecoFakers enderecoFakers = new EnderecoFakers();
+    NumeroFakers numeroFakers = new NumeroFakers();
+
+    @Test
+    public void teste(){
+        System.out.println(internetFakers.getBoolean());
+    }
 
     @Test
     public void nomes(){
@@ -27,18 +34,18 @@ public class FakerTestCase {
     public void emails(){
         String email1 = internetFakers.getEmail1();
         String email2 = internetFakers.getEmail2();
-        String url = internetFakers.getTeste1();
-        String avatar = internetFakers.getTeste2();
-        String macAdress = internetFakers.getTeste3();
-        String senha = internetFakers.getTeste4();
+//        String url = internetFakers.getTeste1();
+//        String avatar = internetFakers.getTeste2();
+//        String macAdress = internetFakers.getTeste3();
+//        String senha = internetFakers.getTeste4();
 
 
         System.out.println("Email 1: " + email1);
         System.out.println("Email 2: " + email2);
-        System.out.println("url: " + url);
-        System.out.println("avatar: " + avatar);
-        System.out.println("macAdress: " + macAdress);
-        System.out.println("senha: " + senha);
+//        System.out.println("url: " + url);
+//        System.out.println("avatar: " + avatar);
+//        System.out.println("macAdress: " + macAdress);
+//        System.out.println("senha: " + senha);
     }
 
     @Test
@@ -57,6 +64,26 @@ public class FakerTestCase {
         System.out.println("Nome estado: " + estado);
         System.out.println("Nome pais: " + pais);
         System.out.println("numero code postal: " + postal);
+    }
+
+    @Test
+    public void numero() {
+//        String numeroDigito = numeroFakers.getNumeroDigit();
+//        String numeroDigitos = numeroFakers.getNumeroDigits();
+//        int numeroRandomDigito = numeroFakers.getNumeroRandomDigit();
+//        int numeroBetween = numeroFakers.getNumberBetween();
+        for(int i = 1; i <= 10; i++){
+//            String numeroDigito = numeroFakers.getNumeroDigit();
+//            String numeroDigitos = numeroFakers.getNumeroDigits();
+//            int numeroRandomDigito = numeroFakers.getNumeroRandomDigit();
+            int numeroBetween = numeroFakers.getNumberBetween();
+            System.out.println(i + " - Numero: " + numeroBetween);
+        }
+
+//        System.out.println("Numero digito: " + numeroDigito);
+//        System.out.println("Numero digitos: " + numeroDigitos);
+//        System.out.println("Numero random digito: " + numeroRandomDigito);
+//        System.out.println("Numero between: " + numeroBetween);
     }
 
 }
